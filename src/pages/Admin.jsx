7c70c70565a2d84846d50fe21ce9d4029955fc90
Wesam Abadi <wisamabady7@gmail.com>
2024-06-06 18:21:52 +0200

    upgraded react v

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,6 +5,18 @@ import "../style/admin.css";
 
 const Admin = () => {
   const [orders, setOrders] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const correctPassword = "root";
+
+  const handlePasswordSubmit = (password) => {
+    if (password === correctPassword) {
+      setIsAuthenticated(true);
+      getOrders();
+    } else {
+      console.log("Incorrect password. Please try again.");
+    }
+  };
 
   const getOrders = async () => {
     const querySnapshot = await getDocs(collection(db, "voiceovers"));
@@ -17,15 +29,32 @@ const Admin = () => {
   };
 
   useEffect(() => {
-    getOrders();
-    console.log("test");
+    console.log("Component loaded");
   }, []);
+
+  if (!isAuthenticated) {
+    return (
+      <div className="page-content admin-container">
+        <div className="password-container content-wrapper">
+          <input
+            className="input input-bordered w-full max-w-xs"
+            type="password"
+            onChange={(e) => {
+              handlePasswordSubmit(e.target.value);
+            }}
+            placeholder="Enter password"
+          />
+        </div>
+      </div>
+    );
+  }
 
   if (orders.length === 0) {
     return <div className="no-orders">No orders</div>;
   }
+
   return (
-    <div className=" page-content admin-container">
+    <div className="page-content admin-container">
       <div className="content-wrapper">
         <table className="orders-table">
           <thead>
